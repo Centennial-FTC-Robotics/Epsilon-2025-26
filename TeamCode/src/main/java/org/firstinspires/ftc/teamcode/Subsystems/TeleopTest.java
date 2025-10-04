78db55852674755fdbd16e08538e73a8af11a597
Syna147 <synasingh14@gmail.com>
2025-10-04 14:21:54 -0400

    Create TeleopTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/TeleopTest.java
@@ -0,0 +1,27 @@
+ package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.subsystems.ColorSensorSubsystem;
+
+@TeleOp(name="Decode Color Sensor Test")
+public class TeleOpColorSensorTest extends LinearOpMode {
+    private ColorSensorSubsystem colorSensor;
+
+    @Override
+    public void runOpMode() {
+        colorSensor = new ColorSensorSubsystem(hardwareMap, "color_sensor");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("R", colorSensor.getRed());
+            telemetry.addData("G", colorSensor.getGreen());
+            telemetry.addData("B", colorSensor.getBlue());
+            telemetry.addData("Alpha", colorSensor.getAlpha());
+
+            telemetry.addData("Detected", colorSensor.getDetectedColor());
+            telemetry.update();
+        }
+    }
+}
