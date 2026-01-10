f3bcf4e8808aec11bed3f257dec07b7cb5733c18
ZackiBoiz <95724562+ZackiBoiz@users.noreply.github.com>
2026-01-10 14:41:00 -0500

    use buttonreader for inputs in tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/FlywheelTuningTutorial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/FlywheelTuningTutorial.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
 import com.arcrobotics.ftclib.gamepad.ButtonReader;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -21,10 +23,18 @@ public class FlywheelTuningTutorial extends OpMode {
     double[] stepSizes = {10.0, 1.0, 0.1, 0.01, 0.001};
     int stepIndex = 1;
 
+    GamepadEx gamepad1Ex;
+    ButtonReader gamepad1BReader;
+    ButtonReader gamepad1YReader;
+
 
     @Override
     public void init(){
+        gamepad1Ex = new GamepadEx(gamepad1);
         flywheelMotor = hardwareMap.get(DcMotorEx.class, "shooter");
+        gamepad1BReader = new ButtonReader(gamepad1Ex, GamepadKeys.Button.B);
+        gamepad1YReader = new ButtonReader(gamepad1Ex, GamepadKeys.Button.Y);
+
         flywheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         flywheelMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
@@ -36,29 +46,25 @@ public class FlywheelTuningTutorial extends OpMode {
     @Override
     public void loop(){
         //gamepad commands, get velocity, update telemetry
-        if (gamepad1.bWasPressed()){
-            if(targetVelocity == highVelocity){
+        if (gamepad1YReader.wasJustPressed()){
+            if (targetVelocity == highVelocity){
                 targetVelocity = lowVelocity;
+            } else{
+                targetVelocity = highVelocity;
             }
-            else{targetVelocity = highVelocity;}
         }
 
-        if(gamepad1.b){
+        if(gamepad1BReader.wasJustPressed()){
             stepIndex = (stepIndex + 1) % stepSizes.length;
         }
 
-        if(gamepad1.dpad_up){
+        if (gamepad1.dpad_up){
             P += stepSizes[stepIndex];
         }
 
-        if(gamepad1.dpad_down){
+        if (gamepad1.dpad_down) {
             P -= stepSizes[stepIndex];
         }
-
-
-
     }
-
-
 }
 
