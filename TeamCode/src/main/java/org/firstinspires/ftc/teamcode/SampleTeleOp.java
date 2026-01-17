acf404f4e13ba5caf23e61977cb1a7ece11c2bc6
ZackiBoiz <95724562+ZackiBoiz@users.noreply.github.com>
2026-01-17 14:37:52 -0500

    use buttoreaders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTeleOp.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.hardware.bosch.BHI260IMU;
@@ -8,7 +11,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.Subsystems.MecanumDrive;
@@ -33,6 +35,25 @@ public class SampleTeleOp extends OpMode {
 
     private MecanumDrive mecanum;
 
+    private GamepadEx gamepad1Ex;
+    private GamepadEx gamepad2Ex;
+
+    private ButtonReader aReader1;
+    private ButtonReader aReader2;
+    private ButtonReader yReader1;
+    private ButtonReader yReader2;
+    private ButtonReader bReader1;
+    private ButtonReader bReader2;
+
+    private ButtonReader dpadLeft1;
+    private ButtonReader dpadLeft2;
+    private ButtonReader dpadRight1;
+    private ButtonReader dpadRight2;
+    private ButtonReader dpadUp1;
+    private ButtonReader dpadUp2;
+    private ButtonReader dpadDown1;
+    private ButtonReader dpadDown2;
+
     // constants
     private final double TURRET_POWER = 0.5;
     private final double INTAKE_POWER = 1.0;
@@ -47,11 +68,6 @@ public class SampleTeleOp extends OpMode {
     private final double PUSH_ANGLE = 6.7;
 
     private double turretYZAngle = 90.0;
-
-    private boolean aPrev1 = false;
-    private boolean aPrev2 = false;
-    private boolean yPrev1 = false;
-    private boolean yPrev2 = false;
 
     @Override
     public void init() {
@@ -89,11 +105,6 @@ public class SampleTeleOp extends OpMode {
         driveFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         driveFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-//        driveBL.setDirection(DcMotor.Direction.REVERSE); // was set
-//        driveBR.setDirection(DcMotor.Direction.REVERSE);
-//        driveFL.setDirection(DcMotor.Direction.REVERSE); // was set
-//        driveFR.setDirection(DcMotor.Direction.REVERSE);
-
         turretXZ.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         flywheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -115,25 +126,53 @@ public class SampleTeleOp extends OpMode {
         turretYZAngle = 90.0;
         turretYZ.turnToAngle(turretYZAngle, AngleUnit.DEGREES);
 
+        gamepad1Ex = new GamepadEx(gamepad1);
+        gamepad2Ex = new GamepadEx(gamepad2);
+
+        aReader1 = new ButtonReader(gamepad1Ex, GamepadKeys.Button.A);
+        aReader2 = new ButtonReader(gamepad2Ex, GamepadKeys.Button.A);
+
+        yReader1 = new ButtonReader(gamepad1Ex, GamepadKeys.Button.Y);
+        yReader2 = new ButtonReader(gamepad2Ex, GamepadKeys.Button.Y);
+
+        bReader1 = new ButtonReader(gamepad1Ex, GamepadKeys.Button.B);
+        bReader2 = new ButtonReader(gamepad2Ex, GamepadKeys.Button.B);
+
+        dpadLeft1 = new ButtonReader(gamepad1Ex, GamepadKeys.Button.DPAD_LEFT);
+        dpadLeft2 = new ButtonReader(gamepad2Ex, GamepadKeys.Button.DPAD_LEFT);
+
+        dpadRight1 = new ButtonReader(gamepad1Ex, GamepadKeys.Button.DPAD_RIGHT);
+        dpadRight2 = new ButtonReader(gamepad2Ex, GamepadKeys.Button.DPAD_RIGHT);
+
+        dpadUp1 = new ButtonReader(gamepad1Ex, GamepadKeys.Button.DPAD_UP);
+        dpadUp2 = new ButtonReader(gamepad2Ex, GamepadKeys.Button.DPAD_UP);
+
+        dpadDown1 = new ButtonReader(gamepad1Ex, GamepadKeys.Button.DPAD_DOWN);
+        dpadDown2 = new ButtonReader(gamepad2Ex, GamepadKeys.Button.DPAD_DOWN);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
     }
 
     @Override
     public void loop() {
-        double lx1 = gamepad1.left_stick_x;
-        double ly1 = -gamepad1.left_stick_y;
-        double rx1 = gamepad1.right_stick_x;
+        readValues();
 
-        double lx2 = gamepad2.left_stick_x;
-        double ly2 = -gamepad2.left_stick_y;
-        double rx2 = gamepad2.right_stick_x;
+        double lx1 = gamepad1Ex.getLeftX();
+        double ly1 = gamepad1Ex.getLeftY();
+        double rx1 = gamepad1Ex.getRightX();
+
+        double lx2 = gamepad2Ex.getLeftX();
+        double ly2 = gamepad2Ex.getLeftY();
+        double rx2 = gamepad2Ex.getRightX();
 
         double lx = (Math.abs(lx1) > STICK_DEADZONE) ? lx1 : lx2;
         double ly = (Math.abs(ly1) > STICK_DEADZONE) ? ly1 : ly2;
         double rx = (Math.abs(rx1) > STICK_DEADZONE) ? rx1 : rx2;
 
-        double rt = Math.max(gamepad1.right_trigger, gamepad2.right_trigger);
+        double rt1 = gamepad1Ex.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+        double rt2 = gamepad2Ex.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER);
+        double rt = Math.max(rt1, rt2);
         double driveScale = 1.0 - (0.75 * rt);
 
         double rotationPower = (Math.abs(rx) > STICK_DEADZONE) ? rx * MAX_ROTATION_POWER : 0.0;
@@ -142,18 +181,21 @@ public class SampleTeleOp extends OpMode {
         mecanum.setMaxSpeed(driveScale);
         mecanum.driveFieldCentric(lx, ly, rotationPower, heading);
 
+        boolean dpadLeft = dpadLeft1.isDown() || dpadLeft2.isDown();
+        boolean dpadRight = dpadRight1.isDown() || dpadRight2.isDown();
+
         double turretCmdXZ = 0.0;
-        if (gamepad1.dpad_left || gamepad2.dpad_left) {
+        if (dpadLeft) {
             turretCmdXZ = -TURRET_POWER;
-        } else if (gamepad1.dpad_right || gamepad2.dpad_right) {
+        } else if (dpadRight) {
             turretCmdXZ = TURRET_POWER;
         } else {
             turretCmdXZ = 0.0;
         }
         turretXZ.setPower(turretCmdXZ);
 
-        boolean dpadUp = gamepad1.dpad_up || gamepad2.dpad_up;
-        boolean dpadDown = gamepad1.dpad_down || gamepad2.dpad_down;
+        boolean dpadUp = dpadUp1.isDown() || dpadUp2.isDown();
+        boolean dpadDown = dpadDown1.isDown() || dpadDown2.isDown();
         if (dpadUp && !dpadDown) {
             turretYZAngle += TURRET_YZ_STEP;
             if (turretYZAngle > TURRET_YZ_MAX) turretYZAngle = TURRET_YZ_MAX;
@@ -164,33 +206,21 @@ public class SampleTeleOp extends OpMode {
             turretYZ.turnToAngle(turretYZAngle, AngleUnit.DEGREES);
         }
 
-        double intakeCmd = 0.0;
-        if (gamepad1.b || gamepad2.b) {
-            intakeCmd = INTAKE_POWER;
-        } else {
-            intakeCmd = 0.0;
-        }
+        boolean intakeOn = bReader1.isDown() || bReader2.isDown();
+        double intakeCmd = intakeOn ? INTAKE_POWER : 0.0;
         intakeMotor.setPower(intakeCmd);
 
-        boolean aNow1 = gamepad1.a;
-        boolean aNow2 = gamepad2.a;
-        boolean aPressed = (aNow1 && !aPrev1) || (aNow2 && !aPrev2);
+        boolean aPressed = aReader1.wasJustPressed() || aReader2.wasJustPressed();
         if (aPressed) {
             // TODO: Implement shoot sequence here (start flywheel, run pusher sequence, etc.)
+            telemetry.addData("Event", "A pressed (shoot) detected");
         }
-        aPrev1 = aNow1;
-        aPrev2 = aNow2;
 
-        boolean yNow1 = gamepad1.y;
-        boolean yNow2 = gamepad2.y;
-        boolean yPressed = (yNow1 && !yPrev1) || (yNow2 && !yPrev2);
+        boolean yPressed = yReader1.wasJustPressed() || yReader2.wasJustPressed();
         if (yPressed) {
             // TODO: Implement auto-aim routine here (use vision / imu / encoders)
+            telemetry.addData("Event", "Y pressed (auto-aim) detected");
         }
-        yPrev1 = yNow1;
-        yPrev2 = yNow2;
-
-        // flywheelMotor.setPower(0.0);
 
         // telemetry
         telemetry.addData("L stick", "lx=%.2f ly=%.2f", lx, ly);
@@ -212,5 +242,23 @@ public class SampleTeleOp extends OpMode {
         intakeMotor.setPower(0.0);
         flywheelMotor.setPower(0.0);
         pusher.turnToAngle(RETRACT_ANGLE, AngleUnit.DEGREES);
+    }
+    
+    private void readValues() {
+        aReader1.readValue();
+        aReader2.readValue();
+        yReader1.readValue();
+        yReader2.readValue();
+        bReader1.readValue();
+        bReader2.readValue();
+
+        dpadLeft1.readValue();
+        dpadLeft2.readValue();
+        dpadRight1.readValue();
+        dpadRight2.readValue();
+        dpadUp1.readValue();
+        dpadUp2.readValue();
+        dpadDown1.readValue();
+        dpadDown2.readValue();
     }
 }
